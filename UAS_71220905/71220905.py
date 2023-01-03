f4c3a6726f6d66c73ef796d5da5529c40ebf20c7
cexcobain666 <117884180+cexcobain666@users.noreply.github.com>
2023-01-03 11:16:45 +0700

    Add files via upload

---
+++ UAS_71220905/71220905.py
@@ -0,0 +1,23 @@
+print ("*****Kredit Keaktifan Mahasiswa*****")
+input ("(student activities credit)")
+print ("1.Menambahkan Kegiatan")
+print ("2.Menampilkan Jumlah Poin")
+print ("3.Keluar")
+print ("------------------------------------")
+
+input =1
+print ("silahkan masukan pilihan anda :")
+input =2
+print ("Nama Kegiatan: HMTI 2022")
+print ("Tanggal Kegiatan: tahun 2022-2023")
+print ("pilihan kategori kegiatan:")
+input =1,2,3
+print ("- prestasi")
+print ("- organisasi")
+print ("- kepanitiaan")
+print ("- rekognisi")
+int ("1,2.3")
+input =3
+input =1,2,3 
+print ("Masukan kategori kegiatan")
+print ("kegiatan berhasil ditambahkan.")
